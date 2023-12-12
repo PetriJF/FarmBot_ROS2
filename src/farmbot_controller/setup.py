a20180c50e9a96a6b3e4832952144b905038e0c2
PetriJF <jamespetri28@gmail.com>
2023-12-12 13:44:12 +0000

    Added the package description

--- src/farmbot_controller/setup.py
+++ src/farmbot_controller/setup.py
@@ -4,7 +4,7 @@ package_name = 'farmbot_controller'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,7 +15,7 @@ setup(
     zip_safe=True,
     maintainer='farmbotdev',
     maintainer_email='jamespetri28@gmail.com',
-    description='TODO: Package description',
+    description='This Package deals with the interface interpreter that receives commands and forwards them as needed to the other packages and nodes.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
