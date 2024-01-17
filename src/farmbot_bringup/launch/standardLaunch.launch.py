172670873de03fbf9ecaa6ba9f96822e1e621795
PetriJF <jamespetri28@gmail.com>
2024-01-17 12:30:49 +0000

    Added launch file

---
+++ src/farmbot_bringup/launch/standardLaunch.launch.py
@@ -0,0 +1,68 @@
+from rclpy.node import Node
+from launch_ros.actions import Node
+from launch import LaunchDescription, LaunchContext, LaunchService
+from launch.actions import RegisterEventHandler
+from launch.events.process import ProcessStarted
+from launch.event_handlers.on_process_start import OnProcessStart
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    controller_node = Node(
+        package = 'farmbot_controllers',
+        executable = 'farmbot_controller',
+        name = 'controller'
+    )
+
+    motor_interp_node = Node(
+        package = 'farmbot_command_handler',
+        executable = 'motor_command_handler',
+        name = 'motor_command_handler'
+    )
+
+    state_interp_node = Node(
+        package = 'farmbot_command_handler',
+        executable = 'state_command_handler',
+        name = 'state_command_handler'
+    )
+
+    device_interp_node = Node(
+        package = 'farmbot_command_handler',
+        executable = 'device_command_handler',
+        name = 'device_command_handler'
+    )
+
+    uart_ctrl_node = Node(
+        package = 'farmbot_command_handler',
+        executable = 'uart_controller',
+        name = 'uart_controller'
+    )
+
+    already_started_nodes = set()
+
+    def start_next_node(event: ProcessStarted, context: LaunchContext):
+        print(f'node {event.process_name} started.')
+        already_started_nodes.update([event.process_name])
+        if len(already_started_nodes) == 4:
+            print(f'all required nodes are up, starting uart_controller')
+            return uart_ctrl_node
+
+    return LaunchDescription([
+        RegisterEventHandler(event_handler = OnProcessStart(target_action = controller_node,
+                                                            on_start = start_next_node)),
+        RegisterEventHandler(event_handler = OnProcessStart(target_action = motor_interp_node,
+                                                            on_start = start_next_node)),
+        RegisterEventHandler(event_handler = OnProcessStart(target_action = state_interp_node,
+                                                            on_start = start_next_node)),
+        RegisterEventHandler(event_handler = OnProcessStart(target_action = device_interp_node,
+                                                            on_start = start_next_node)),
+        controller_node,
+        motor_interp_node,
+        state_interp_node,
+        device_interp_node,
+    ])
+
+if __name__ == '__main__':
+    ls = LaunchService()
+    ls.include_launch_description(generate_launch_description())
+    ls.run()
