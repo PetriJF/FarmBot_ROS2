156ffea9cb6820b0ce0952ca64feb608a0e74ccf
PetriJF <jamespetri28@gmail.com>
2024-02-29 15:17:37 +0000

    Clean up

--- src/farmbot_controllers/farmbot_controllers/conf_mng_servers.py
+++ src/farmbot_controllers/farmbot_controllers/conf_mng_servers.py
@@ -2,23 +2,26 @@ import rclpy
 from rclpy.node import Node
 from farmbot_interfaces.msg import ParameterList, MapCommand
 from ament_index_python.packages import get_package_share_directory
+from std_msgs.msg import String
+from farmbot_interfaces.msg import ParameterCommand
+from farmbot_interfaces.srv import ParameterConfig, LoadParamConfig
 
 import os
 import time
 import yaml
 
-from std_msgs.msg import String
-from farmbot_interfaces.msg import ParameterCommand
-from farmbot_interfaces.srv import ParameterConfig, LoadParamConfig
-
 class ConfigServer(Node):
+    '''
+    Node handling the parameter recording and loading onto the farmduino
+    '''
     # Node contructor
     def __init__(self):
-        super().__init__("ConfigServer")
-        self.firmwareInitialized_ = False
+        super().__init__('ConfigServer')
+        # Flag waiting for initialization to be done before the config is loaded
+        self.firmware_init_done_ = False
 
         # The dictionary containing all of the parameters for the farmbot
-        self.parameterValues = {
+        self.param_vals = {
             ParameterList.PARAM_CONFIG_OK : 0,
             ParameterList.PARAM_USE_EEPROM : 0,
             ParameterList.PARAM_E_STOP_ON_MOV_ERR : 0,
@@ -137,169 +140,168 @@ class ConfigServer(Node):
             ParameterList.PIN_GUARD_5_ACTIVE_STATE : 0
         }
         
-        self.defaultPath_ = os.path.join(
+        self.default_path_ = os.path.join(
             get_package_share_directory('farmbot_controllers'),
             'config'
         )
-        self.defaultFirmwareConfig_ = 'firmwareDefault.yaml'
-        self.defaultConfig_ = 'defaultParameterConfig.yaml'
-        self.labConfig_ = 'labFB.yaml'
-        self.activeConfig_ = 'activeConfig.yaml'
+        self.base_config_ = 'firmwareDefault.yaml'          # default config loaded by the firmware
+        self.default_config_ = 'defaultParameterConfig.yaml'
+        self.lab_fb_config_ = 'labFB.yaml'
+        self.active_config_ = 'activeConfig.yaml'
 
-        self.configServer_ = self.create_service(ParameterConfig, 'manage_param_config', self.configRequestCallback)
-        self.loadingServer_ = self.create_service(LoadParamConfig, 'load_param_config', self.parameterLoadingRequest)
+        self.config_server_ = self.create_service(ParameterConfig, 'manage_param_config', self.config_request_server)
+        self.config_loading_server_ = self.create_service(LoadParamConfig, 'load_param_config', self.param_loading_server)
 
-        self.paramCmd_ = ParameterCommand()
-        self.paramCmdPub_ = self.create_publisher(ParameterCommand, 'parameter_command', 10)
+        self.param_cmd_ = ParameterCommand()
+        self.param_cmd_pub_ = self.create_publisher(ParameterCommand, 'parameter_command', 10)
 
         # UART Rx Subscriber
-        self.uartRxSub_ = self.create_subscription(String, 'uart_receive', self.farmbotFeedbackCallback, 10)
+        self.uart_rx_sub_ = self.create_subscription(String, 'uart_receive', self.uart_rx_callback, 10)
 
         # Map updating publisher
-        self.mapCommand_ = MapCommand()
-        self.mapCmdPub_ = self.create_publisher(MapCommand, 'map_cmd', 10)
+        self.map_cmd_ = MapCommand()
+        self.map_cmd_pub_ = self.create_publisher(MapCommand, 'map_cmd', 10)
 
         # Log the initialization
-        self.get_logger().info("Config Server Initialized..")
+        self.get_logger().info('Config Server Initialized..')
 
     ## UART Handling Callback
-    def farmbotFeedbackCallback(self, msg = String):
-        msgSplit = (msg.data).split(' ')
-        reportCode = msgSplit[0]
-        if msg.data == 'R99 ARDUINO STARTUP COMPLETE' and not self.firmwareInitialized_:
-            self.firmwareInitialized_ = True
-            self.retrieveConfig()
+    def uart_rx_callback(self, msg: String):
+        msg_split = (msg.data).split(' ')
+        reportCode = msg_split[0]
+        if msg.data == 'R99 ARDUINO STARTUP COMPLETE' and not self.firmware_init_done_:
+            self.firmware_init_done_ = True
+            self.retrieve_config()
         if reportCode == 'R21' or reportCode == 'R23':
-            self.set_value(int(float(msgSplit[1][1:])), int(float(msgSplit[2][1:])))
-            self.get_logger().info(f"Updated parameter {msgSplit[1]} to {msgSplit[2]}")
+            self.set_value(int(float(msg_split[1][1:])), int(float(msg_split[2][1:])))
+            self.get_logger().info(f'Updated parameter {msg_split[1]} to {msg_split[2]}')
 
-    def retrieveConfig(self):
-        activeConfig = os.path.join(self.defaultPath_, self.activeConfig_)
-        if os.path.exists(activeConfig):
-            self.load_from_yaml(self.defaultPath_, self.activeConfig_)
-            self.loadParameters()
-            self.get_logger().info("Initialized with active config from previous run")
+    def retrieve_config(self):
+        active_config_path = os.path.join(self.default_path_, self.active_config_)
+        if os.path.exists(active_config_path):
+            self.load_from_yaml(self.default_path_, self.active_config_)
+            self.load_params()
+            self.get_logger().info('Initialized with active config from previous run')
         else:
-            self.get_logger().warn("Previous config could not be found! You will need to initialize the appropriate parameter config")
+            self.get_logger().warn('Previous config could not be found! You will need to initialize the appropriate parameter config')
 
-    def save_to_yaml(self, path = '', fileName = ''):
+    def save_to_yaml(self, path = '', file_name = ''):
         if path == '':
-            self.get_logger().warn("Path not set for retrieving the parameter config file")
+            self.get_logger().warn('Path not set for retrieving the parameter config file')
             return
-        if fileName == '':
-            self.get_logger().warn("Parameter Config File name not set")
+        if file_name == '':
+            self.get_logger().warn('Parameter Config File name not set')
             return
         if not os.path.exists(path):
-            self.get_logger().warn("File path is invalid")
+            self.get_logger().warn('File path is invalid')
             return
 
-        self.get_logger().info(f"Saving current parameter configuration at {os.path.join(path, fileName)}")
+        self.get_logger().info(f'Saving current parameter configuration at {os.path.join(path, file_name)}')
             
-        with open(os.path.join(path, fileName), 'w') as yaml_file:
-            yaml.dump(self.parameterValues, yaml_file, default_flow_style = False)
+        with open(os.path.join(path, file_name), 'w') as yaml_file:
+            yaml.dump(self.param_vals, yaml_file, default_flow_style = False)
 
-    def load_from_yaml(self, path = '', fileName = ''):
+    def load_from_yaml(self, path = '', file_name = ''):
         if path == '':
-            self.get_logger().warn("Path not set for retrieving the parameter config file")
+            self.get_logger().warn('Path not set for retrieving the parameter config file')
             return
-        if fileName == '':
-            self.get_logger().warn("Parameter Config File name not set")
+        if file_name == '':
+            self.get_logger().warn('Parameter Config File name not set')
             return
         if not os.path.exists(path):
-            self.get_logger().warn("File path is invalid")
+            self.get_logger().warn('File path is invalid')
             return
         
-        with open(os.path.join(path, fileName), 'r') as yaml_file:
+        with open(os.path.join(path, file_name), 'r') as yaml_file:
             loaded_data = yaml.safe_load(yaml_file)
             if isinstance(loaded_data, dict):
-                self.parameterValues = loaded_data
+                self.param_vals = loaded_data
             else:
-                self.get_logger().warn("Invalid YAML file format..")
+                self.get_logger().warn('Invalid YAML file format..')
 
     def set_value(self, param, value):
-        self.get_logger().info(f"Set parameter {param} to {value}")
-        self.parameterValues[param] = value
+        self.get_logger().info(f'Set parameter {param} to {value}')
+        self.param_vals[param] = value
     
     def get_value(self, param):
-        return self.parameterValues[param]
+        return self.param_vals[param]
 
-    def parameterLoadingRequest(self, request, response):
+    def param_loading_server(self, request, response):
         # A standard config that works for all farmbots
         if request.standard: 
-            self.load_from_yaml(path = self.defaultPath_, fileName = self.defaultConfig_)
-            self.loadParameters()
+            self.load_from_yaml(path = self.default_path_, file_name = self.default_config_)
+            self.load_params()
         # A configuration more specific to the model you are running
         else:
             ver = request.ver ## TODO add support for multiple configurations
-            self.load_from_yaml(path = self.defaultPath_, fileName = self.labConfig_)
-            self.loadParameters()
+            self.load_from_yaml(path = self.default_path_, file_name = self.lab_fb_config_)
+            self.load_params()
 
         response.success = True
         return response
 
-    def loadParameters(self):
-        self.paramCmd_.list = False
-        self.paramCmd_.write = True
-        self.paramCmd_.read = False
-        self.paramCmd_.update = False
+    def load_params(self):
+        self.param_cmd_.list = False
+        self.param_cmd_.write = True
+        self.param_cmd_.read = False
+        self.param_cmd_.update = False
 
         # Loading only the parameters that are not loaded to the desired value (Greatly increases upload speed)
-        with open(os.path.join(self.defaultPath_, self.defaultFirmwareConfig_), 'r') as yaml_file:
+        with open(os.path.join(self.default_path_, self.base_config_), 'r') as yaml_file:
             loaded_firmware_config = yaml.safe_load(yaml_file)
-            for key, value in self.parameterValues.items():
+            for key, value in self.param_vals.items():
                 if(loaded_firmware_config[key] != value):
-                    self.paramCmd_.param = key
-                    self.paramCmd_.value = value
-                    self.paramCmdPub_.publish(self.paramCmd_)
+                    self.param_cmd_.param = key
+                    self.param_cmd_.value = value
+                    self.param_cmd_pub_.publish(self.param_cmd_)
                     time.sleep(0.1)
-                    #self.get_logger().info(f"F22 P{key} V{value}")
 
-        self.get_logger().info("Parameter loading complete!")
+        self.get_logger().info('Parameter loading complete!')
 
-    def configRequestCallback(self, request, response):
+    def config_request_server(self, request, response):
         response.success = True    # success until proven otherwise
         response.cmd = request.data
-        msgSplit = (request.data).split(' ')
-        code = msgSplit[0]
+        msg_split = (request.data).split(' ')
+        code = msg_split[0]
         # Using both command and report codes so that the commands themselves
         # can be just fed into it with ease
         if code == 'R21' or code == 'R23' or code == 'F22' or code == 'F23':
-            self.set_value(int(msgSplit[1][1:]), int(msgSplit[2][1:]))
+            self.set_value(int(msg_split[1][1:]), int(msg_split[2][1:]))
             response.value = 0
             return response
         if code == 'F21':
-            response.value = self.get_value(msgSplit[1][1:])
+            response.value = self.get_value(msg_split[1][1:])
             return response
 
         # Requests with non farmbot commands
         if code == 'S': # Format S PARAM_INDEX PARAM_VALUE. e.g. S 2 1
-            self.set_value(msgSplit[1], msgSplit[2])
+            self.set_value(msg_split[1], msg_split[2])
             
             return response
         if code == 'G':
-            response.value = self.get_value(msgSplit[1])
+            response.value = self.get_value(msg_split[1])
             return response
         if code == 'MAP':
             response.value = 0
-            self.mapCommand_.sort = False
-            self.mapCommand_.reindex = False
-            self.mapCommand_.back_up = False
-            self.mapCommand_.update = True
-            self.mapCommand_.update_info = [
-                'X ' + str(self.parameterValues[ParameterList.MOVEMENT_AXIS_NR_STEPS_X] / 
-                                self.parameterValues[ParameterList.MOVEMENT_STEP_PER_MM_X]),
-                'Y ' + str(self.parameterValues[ParameterList.MOVEMENT_AXIS_NR_STEPS_Y] / 
-                                self.parameterValues[ParameterList.MOVEMENT_STEP_PER_MM_Y]),
-                'Z ' + str(self.parameterValues[ParameterList.MOVEMENT_AXIS_NR_STEPS_Z] / 
-                                self.parameterValues[ParameterList.MOVEMENT_STEP_PER_MM_Z]),
+            self.map_cmd_.sort = False
+            self.map_cmd_.reindex = False
+            self.map_cmd_.back_up = False
+            self.map_cmd_.update = True
+            self.map_cmd_.update_info = [
+                'X ' + str(self.param_vals[ParameterList.MOVEMENT_AXIS_NR_STEPS_X] / 
+                                self.param_vals[ParameterList.MOVEMENT_STEP_PER_MM_X]),
+                'Y ' + str(self.param_vals[ParameterList.MOVEMENT_AXIS_NR_STEPS_Y] / 
+                                self.param_vals[ParameterList.MOVEMENT_STEP_PER_MM_Y]),
+                'Z ' + str(self.param_vals[ParameterList.MOVEMENT_AXIS_NR_STEPS_Z] / 
+                                self.param_vals[ParameterList.MOVEMENT_STEP_PER_MM_Z]),
             ]
             
-            self.mapCmdPub_.publish(self.mapCommand_)
+            self.map_cmd_pub_.publish(self.map_cmd_)
             return response
         if code == 'SAVE':
             response.value = 0
-            os.makedirs(os.path.dirname(os.path.join(self.defaultPath_, self.activeConfig_)), exist_ok=True)
-            self.save_to_yaml(self.defaultPath_, self.activeConfig_)
+            os.makedirs(os.path.dirname(os.path.join(self.default_path_, self.active_config_)), exist_ok=True)
+            self.save_to_yaml(self.default_path_, self.active_config_)
             return response
 
         # If the service gets here, the request could not be processed
