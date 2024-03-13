e425a89051074b888fc5b2d5e85b49084bd8f743
PetriJF <jamespetri28@gmail.com>
2024-03-13 17:04:46 +0000

    clean up

--- src/camera_handler/camera_handler/luxonis_publisher.py
+++ src/camera_handler/camera_handler/luxonis_publisher.py
@@ -11,12 +11,19 @@ import depthai as dai
 import threading
 
 class CameraNode:
+    '''
+    Camera module that reads the Luxonis camera packets and publishes the RGB and 
+    depth frames on their respective topics
+    '''
     def __init__(self, node: Node):
+        '''
+        Module constructor extendint the node instance
+        '''
         self.node_= node
         
-        self.bridge = CvBridge()  # Bridge to convert between ROS and OpenCV images
-        self.load_config()  # Load configuration from YAML file
-        self.setup_camera()  # Initialize and configure the DepthAI camera
+        self.bridge = CvBridge()# Bridge to convert between ROS and OpenCV images
+        self.load_config()      # Load configuration from YAML file
+        self.setup_camera()     # Initialize and configure the DepthAI camera
         
         # Initialize publishers for RGB and depth images
         self.rgb_publisher = self.node_.create_publisher(Image, 'rgb_img', 10)
@@ -30,7 +37,44 @@ class CameraNode:
         self.processing_thread.daemon = True  # Ensures that the thread will close when the main program exits
         self.processing_thread.start()
     
+    def setup_camera(self):
+        '''
+        Sets the camera links and pipeline
+        '''
+        # Configure the DepthAI pipeline with stereo and RGB camera nodes
+        self.pipeline = dai.Pipeline()
+        stereo, mono_left, mono_right, cam_rgb = self.configure_depthai_components()
+        
+        # Link camera outputs to XLinkOut nodes for transmission to host
+        mono_left.out.link(stereo.left)
+        mono_right.out.link(stereo.right)
+        cam_rgb.preview.link(self.xout_rgb.input)
+        stereo.disparity.link(self.xout_stereo.input)
+        
+        self.device = dai.Device(self.pipeline)  # Connect to the DepthAI device
+
+    def load_config(self):
+        '''
+        Loads the camera configuration from the calibration file
+        '''
+        # Load configuration from YAML file
+        self.config_directory_ = os.path.join(get_package_share_directory('camera_handler'), 'config')
+        config_file = 'camera_config.yaml'
+        self.config_data = self.load_from_yaml(self.config_directory_, config_file)
+        # Extract relevant configuration values
+        self.HOST = self.config_data['server_constants']['HOST']
+        self.PORT = self.config_data['server_constants']['PORT']
+        self.DEPTH_MIN_VAL = self.config_data['camera_constants']['DEPTH_MIN_VAL']
+        self.DEPTH_MAX_VAL = self.config_data['camera_constants']['DEPTH_MAX_VAL']
+        self.FOCAL_LENGTH_PX = self.config_data['camera_constants']['FOCAL_LENGTH_PX']
+        self.BASELINE = self.config_data['camera_constants']['BASELINE']
+
+        self.node_.get_logger().info('Updated camera settings based on calibration')
+
     def packet_processing_loop(self):
+        '''
+        Loop used for processing packets from the camera
+        '''
         while True:
             # Fetch packets from the device's output queues
             latestPacket = {"rgb": None, "stereo": None}
@@ -48,46 +92,21 @@ class CameraNode:
                 self.depth_image_ = self.process_depth_frame(latestPacket["stereo"].getFrame())
                 self.publish_images(depth_frame=self.depth_image_)
 
-    def load_config(self):
-        # Load configuration from YAML file
-        self.config_directory_ = os.path.join(get_package_share_directory('camera_handler'), 'config')
-        config_file = 'camera_config.yaml'
-        self.config_data = self.load_from_yaml(self.config_directory_, config_file)
-        # Extract relevant configuration values
-        self.HOST = self.config_data['server_constants']['HOST']
-        self.PORT = self.config_data['server_constants']['PORT']
-        self.DEPTH_MIN_VAL = self.config_data['camera_constants']['DEPTH_MIN_VAL']
-        self.DEPTH_MAX_VAL = self.config_data['camera_constants']['DEPTH_MAX_VAL']
-        self.FOCAL_LENGTH_PX = self.config_data['camera_constants']['FOCAL_LENGTH_PX']
-        self.BASELINE = self.config_data['camera_constants']['BASELINE']
-
-        self.node_.get_logger().info('Updated camera settings based on calibration')
-
-    def setup_camera(self):
-        # Configure the DepthAI pipeline with stereo and RGB camera nodes
-        self.pipeline = dai.Pipeline()
-        stereo, monoLeft, monoRight, cam_rgb = self.configure_depthai_components()
-        
-        # Link camera outputs to XLinkOut nodes for transmission to host
-        monoLeft.out.link(stereo.left)
-        monoRight.out.link(stereo.right)
-        cam_rgb.preview.link(self.xout_rgb.input)
-        stereo.disparity.link(self.xout_stereo.input)
-        
-        self.device = dai.Device(self.pipeline)  # Connect to the DepthAI device
-
     def configure_depthai_components(self):
+        '''
+        Configuration for the DepthAI components
+        '''
         # Create and configure DepthAI components (stereo depth, mono cameras, RGB camera)
         stereo = self.pipeline.create(dai.node.StereoDepth)
-        monoLeft = self.pipeline.create(dai.node.MonoCamera)
-        monoRight = self.pipeline.create(dai.node.MonoCamera)
+        mono_left = self.pipeline.create(dai.node.MonoCamera)
+        mono_right = self.pipeline.create(dai.node.MonoCamera)
         cam_rgb = self.pipeline.create(dai.node.ColorCamera)
         
         # Mono camera settings
-        monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-        monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+        mono_left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        mono_left.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        mono_right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        mono_right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
         
         # RGB camera settings
         cam_rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
@@ -106,9 +125,12 @@ class CameraNode:
         # Configure stereo depth settings based on YAML configuration
         self.configure_stereo_depth(stereo)
         
-        return stereo, monoLeft, monoRight, cam_rgb
+        return stereo, mono_left, mono_right, cam_rgb
 
     def configure_stereo_depth(self, stereo):
+        '''
+        Apply stereo depth settings from configuration amd disparity post-processing
+        '''
         # Apply stereo depth settings from configuration
         stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
         stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
@@ -123,7 +145,9 @@ class CameraNode:
         stereo.initialConfig.set(config)
 
     def publish_images(self, rgb_frame = None, depth_frame = None):
-        # Publish RGB and depth frames if available
+        '''
+        Publish RGB and depth frames if available
+        '''
         if rgb_frame is not None:
             rgb_msg = self.bridge.cv2_to_imgmsg(rgb_frame, encoding='bgr8')
             self.rgb_publisher.publish(rgb_msg)
@@ -156,21 +180,26 @@ class CameraNode:
         
 
     def process_depth_frame(self, disparity_frame):
-        # Process disparity frame to generate a depth frame for publishing
+        '''
+        Process disparity frame to generate a depth frame for publishing
+        '''
         disparity_frame[disparity_frame == 0] = 0.01  # Handle zero disparity
         depth_frame = (self.FOCAL_LENGTH_PX * self.BASELINE) / disparity_frame
         depth_frame[(depth_frame >= self.DEPTH_MAX_VAL) | (depth_frame <= self.DEPTH_MIN_VAL)] = 0
         scaled_frame = np.clip(((depth_frame - 0) / (self.DEPTH_MAX_VAL - 0) * 255.0), 0, 255).astype(np.uint8)
         return scaled_frame
 
-    def load_from_yaml(self, path, fileName):
-        # Load configuration data from a YAML file
-        fullPath = os.path.join(path, fileName)
-        if not os.path.exists(fullPath):
-            self.node_.get_logger().warn(f"File path is invalid: {fullPath}")
+    def load_from_yaml(self, path, file_name):
+        '''
+        Loads the specified yaml file from the specified path and returns a 
+        the dictionary withign the file
+        '''
+        full_path = os.path.join(path, file_name)
+        if not os.path.exists(full_path):
+            self.node_.get_logger().warn(f"File path is invalid: {full_path}")
             return None
         
-        with open(fullPath, 'r') as yaml_file:
+        with open(full_path, 'r') as yaml_file:
             try:
                 return yaml.safe_load(yaml_file)
             except yaml.YAMLError as e:
