3bdf3ca921a276dd7f80c8e062ada80d7f2649b1
PetriJF <jamespetri28@gmail.com>
2024-05-09 11:14:36 +0100

    Added command for inverting the encoder direction

--- src/farmbot_controllers/farmbot_controllers/keyboard_teleop.py
+++ src/farmbot_controllers/farmbot_controllers/keyboard_teleop.py
@@ -33,7 +33,7 @@ class KeyboardTeleOp(Node):
                      'P_3', 'P_4', 'I_0', 'I_1', 'I_2', 'D_C', 'D_L_1', 'D_L_0',
                      'D_W_1', 'D_W_0', 'D_V_1', 'D_V_0',
                      'H_0', 'H_1')
-        compound_cmds = ('C_0', 'P_1', 'P_2', 'C_1', 'T_1_0', 'T_2_0', 'T_3_0', 'S_1_0', 'S_2_0', 'S_3_0', 'M', 'CONF', 'H_2')
+        compound_cmds = ('C_0', 'P_1', 'P_2', 'C_1', 'C_2', 'T_1_0', 'T_2_0', 'T_3_0', 'S_1_0', 'S_2_0', 'S_3_0', 'M', 'CONF', 'H_2')
         # Record the user input
         user_input = input('\nEnter command: ')
         
