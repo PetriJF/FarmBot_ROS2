635a765364e4e427d8d4f6d79bb2551752347738
AURAProj <aura.team.gh@gmail.com>
2025-05-27 15:34:29 +0100

    initial commit for ms camera

--- src/camera_handler/setup.py
+++ src/camera_handler/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 import os
+import glob
 package_name = 'camera_handler'
 
 setup(
@@ -10,8 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), [os.path.join(package_name, 'config', 'luxonis_camera_config.yaml')]),
-        (os.path.join('share', package_name, 'config'), [os.path.join(package_name, 'config', 'standard_camera_config.yaml')]),
+        (os.path.join('share', package_name, 'config'), glob.glob(f'{package_name}/config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -26,6 +26,7 @@ setup(
             'luxonis_camera = camera_handler.luxonis_camera:main',
             'standard_camera = camera_handler.standard_camera:main',
             'realsense_405_camera = camera_handler.realsense_405_camera:main',
+            'luxonis_ms_camera = camera_handler.luxonis_ms_camera:main',
         ],
     },
 )
