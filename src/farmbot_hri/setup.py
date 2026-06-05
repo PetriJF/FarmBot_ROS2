2a55be7232693e4b8d2b00f1e2aac711624e9e6d
Salome <salome.deoliveira.2026@mumail.ie>
2026-06-05 11:36:28 +0100

    Description added to the setup.py file from farmbot_hri package

--- src/farmbot_hri/setup.py
+++ src/farmbot_hri/setup.py
@@ -1,23 +1,24 @@
 from setuptools import find_packages, setup
+import glob
 import os
 package_name = 'farmbot_hri'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), [os.path.join(package_name, 'config', 'AutonomousCommand.yaml')])
+        (os.path.join('share', package_name, 'config'), glob.glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='aura_ws',
+    maintainer='Salome',
     maintainer_email='salome.deoliveira.2026@mumail.ie',
     description='Package containing the Keyboard and Autonomous controllers for the HRI',
-    license='TODO: License declaration',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
