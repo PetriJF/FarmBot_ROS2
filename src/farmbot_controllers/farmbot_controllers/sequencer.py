0bf4bc835b2083e3c5cf12e95501294e5d83b8ef
Salome <salome.deoliveira.2026@mumail.ie>
2026-06-19 15:47:41 +0100

    Undoing the last change because it was redundant

--- src/farmbot_controllers/farmbot_controllers/sequencer.py
+++ src/farmbot_controllers/farmbot_controllers/sequencer.py
@@ -418,5 +418,3 @@ class Sequencer:
     def status_callback(self, state: Bool):
         """Transmit the busy statefrom the Uart controller to the sequencer timer."""
         self.farmbot_estop = state.data
-        if self.farmbot_estop:
-            self.clear_sequence()
