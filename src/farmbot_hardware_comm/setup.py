a0a6d412fc6995f7bfae160262e0a30da35944f4
Salome <salome.deoliveira.2026@mumail.ie>
2026-06-19 16:06:04 +0100

    Update versions

--- src/farmbot_hardware_comm/setup.py
+++ src/farmbot_hardware_comm/setup.py
@@ -12,7 +12,7 @@ package_name = 'farmbot_hardware_comm'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
