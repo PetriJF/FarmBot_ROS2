d3843e1abddf3c32a0cf1e6f4e00d7ce42b7f020
Salome <salome.deoliveira.2026@mumail.ie>
2026-06-25 16:30:11 +0100

    Version updates.

--- src/farmbot_controllers/setup.py
+++ src/farmbot_controllers/setup.py
@@ -12,7 +12,7 @@ package_name = 'farmbot_controllers'
 
 setup(
     name=package_name,
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
