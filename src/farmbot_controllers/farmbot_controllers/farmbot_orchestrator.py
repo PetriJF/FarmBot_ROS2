038a39593f086caf504d334a0410a2e1e25bcad2
Salome <salome.deoliveira.2026@mumail.ie>
2026-06-26 15:05:23 +0100

    Change the variable name previous_cmd to current_cmd

--- src/farmbot_controllers/farmbot_controllers/farmbot_orchestrator.py
+++ src/farmbot_controllers/farmbot_controllers/farmbot_orchestrator.py
@@ -42,7 +42,7 @@ class FarmbotOrchestrator(Node):
                                                     self.farmbot_command_callback, 10)
 
         self.busy_state = False
-        self.previous_cmd = ''
+        self.current_cmd = ''
 
         # Farmbot state publisher
         self.estop_pressed = Bool()
@@ -92,16 +92,16 @@ class FarmbotOrchestrator(Node):
 
                 if command in ['E', 'F09']:
                     self.queue['non_priority_cmd'].clear()
-                elif self.previous_cmd:
-                    self.queue['non_priority_cmd'] = ([self.previous_cmd]
+                elif self.current_cmd_cmd:
+                    self.queue['non_priority_cmd'] = ([self.current_cmd]
                                                       + self.queue['non_priority_cmd'])
 
                 self.queue['priority_cmd'].clear()
                 self.send_goal(command)
 
         elif self.queue['non_priority_cmd'] and not self.busy_state:
-            self.previous_cmd = self.queue['non_priority_cmd'].pop(0)
-            self.send_goal(self.previous_cmd)
+            self.current_cmd = self.queue['non_priority_cmd'].pop(0)
+            self.send_goal(self.current_cmd)
 
         else:
             return
@@ -150,13 +150,13 @@ class FarmbotOrchestrator(Node):
             self.get_logger().info('The current command has been canceled by a estop request')
 
         if status == 'ABORTED':
-            self.get_logger().info('The current command has been aborted.')
+            self.get_logger().info('The Farmbot has been paused.')
 
         elif status == 'ERROR':
             self.get_logger().info('Something happened the command has finished with error.')
 
         elif status == 'SUCCEED':
-            self.previous_cmd = ''
+            self.current_cmd = ''
             self.get_logger().info('The command was successful and has been completed')
 
         self.busy_state = False
